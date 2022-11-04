be75a0d57c1e13f91e70d693c5f9357900d8cad1
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-11-04 07:13:24 +0100

    google analytics added

--- Controllers/apartmentController.js
+++ Controllers/apartmentController.js
@@ -298,31 +298,27 @@ exports.getDistances = catchAsync(async (req, res, next) => {
 
 const {google}= require('googleapis')
 const scopes= 'https://www.googleapis.com/auth/analytics.readonly'
-const jwt= new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIVATE_KEY,scopes)
+const jwt= new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),scopes)
 
+const view_id= '279016709'
 
 exports.googleAnalytics= async(req, res, next)=>{
-  try{
+
     const result= await google.analytics('v3').data.ga.get({
       'auth': jwt,
-      'ids': 'ga:338601947',
+      'ids': 'ga:'+ view_id,
       'start-date': '30daysAgo',
       'end-date':'today',
       'metrics': 'ga:pageviews'
     })
 
+    console.log(result)
+
+    if(!result) return next(new AppError('something went wrong', 400))
     res.status(200).json({
       status: 'success',
       data: result
 
     })
-  }catch(err){
-    console.log(err)
-    res.status(500).json({
-      status: "fail",
-      data: err
-    })
-
-  }
 }
   
